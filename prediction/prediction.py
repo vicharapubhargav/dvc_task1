3262a47bace4a9ee26ffa4abcc2db5d490961e38
Bhargav Vicharapu <vicharapubhargav462981609@gmail.com>
2021-09-16 00:25:17 +0530

    Model Build Complete

---
+++ prediction/prediction.py
@@ -0,0 +1,77 @@
+import yaml
+import os
+import json
+import joblib
+import numpy as np
+from  src.get_data import read_params
+from pickle import load
+
+params_path = "params.yaml"
+schema_path = os.path.join("prediction", "schema_in.json")
+scaler = load(open('scaler.pkl', 'rb'))
+
+class NotInRange(Exception):
+    def __init__(self, col,message="Values entered are not in expected range"):
+        self.message = col+" "+message
+        super().__init__(self.message)
+
+
+def predict(data):
+    config = read_params(params_path)
+    model_dir_path = config["webapp_model_dir"]
+    model = joblib.load(model_dir_path)
+    prediction = model.predict(scaler.transform(data)).tolist()[0]
+    print(prediction)
+    try:
+        if 0 <= prediction <= 10:
+            return prediction
+        else:
+            raise NotInRange
+    except NotInRange:
+        return "Unexpected result"
+
+
+def get_schema(schema_path=schema_path):
+    with open(schema_path) as json_file:
+        schema = json.load(json_file)
+    return schema
+
+def validate_input(dict_request):
+
+    def _validate_values(col, val):
+        schema = get_schema()
+
+        if not (schema[col]["min"] <= float(val) <= schema[col]["max"]) :
+            raise NotInRange(col)
+            
+
+    for col, val in dict_request.items():
+        _validate_values(col, val)
+    
+    return True
+
+
+def form_response(dict_request):
+   if validate_input(dict_request):
+        print("HI")
+        data = dict_request.values()
+        data = [list(map(float, data))]
+        response = predict(data)
+        return response
+
+    
+def api_response(dict_request):
+    try:
+        if validate_input(dict_request):
+            data = np.array([list(dict_request.values())])
+            response = predict(data)
+            response = {"response": response}
+            return response
+            
+    except NotInRange as e:
+        response = {"the_exected_range": get_schema(), "response": str(e) }
+        return response
+
+    except Exception as e:
+        response = {"response": str(e) }
+        return response
--- src/EDA.py
+++ src/EDA.py
@@ -2,9 +2,11 @@
 # save it in the data/raw for further process
 import os
 import pandas as pd
-from get_data import read_params, get_data
+from get_data import read_params
 import argparse
 from sklearn.preprocessing import StandardScaler
+from pickle import dump
+
 
 
 def eda_process2(df,config_path):
@@ -12,35 +14,37 @@ def eda_process2(df,config_path):
     target = config["base"]["target_col"]
 
     X = df.drop(target,axis=1)
-
     scaler=StandardScaler()
     arr=scaler.fit_transform(X)
-    column_name = [df.columns]
     y=df[[target]]
-
+    dump(scaler, open('scaler.pkl', 'wb'))
     df2=pd.DataFrame(arr)
     df2[target]=y
-    df2.columns=column_name
+    df2.columns=df.columns
     return df2
 
 
+
 def eda_process1(df,config_path):
-    df['GRE Score']=df['GRE Score'].fillna(df['GRE Score'].mean())
-    df['TOEFL Score']=df['TOEFL Score'].fillna(df['TOEFL Score'].mean())
-    df['University Rating']=df['University Rating'].fillna(df['University Rating'].mean())
+    df['GRE_Score']=df['GRE_Score'].fillna(df['GRE_Score'].mean())
+    df['TOEFL_Score']=df['TOEFL_Score'].fillna(df['TOEFL_Score'].mean())
+    df['University_Rating']=df['University_Rating'].fillna(df['University_Rating'].mean())
     
-    df.drop(columns=['Serial No.'],inplace=True)
+    df.drop(columns=['Serial_No.'],inplace=True)
     df2=eda_process2(df,config_path)
     
     return df2
 
 def process_df(config_path):
     config = read_params(config_path)
-    df = get_data(config_path)
-    new_cols = [col.replace(" ", "_") for col in df.columns]
-    raw_data_path = config["process_data"]["raw_dataset_csv"]
+    data_path = config["load_data"]["raw_dataset_csv"]
+    raw_data_path=config["process_data"]["raw_dataset_csv"]
+    df = pd.read_csv(data_path,sep=',')
     df2=eda_process1(df,config_path)
+    print(df2.columns)
     df2.to_csv(raw_data_path, sep=",", index=False)
+    
+
 
 
 if __name__=="__main__":
